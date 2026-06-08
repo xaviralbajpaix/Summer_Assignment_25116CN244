a68d90a8ddc20029d8d5dadc0ef921f49860fe15
Aviral Bajpai <xaviralbajpaix@gmail.com>
2026-06-08 21:47:12 +0530

    Day 8 Assignment

---
+++ Day-8/program29.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class program29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
---
+++ Day-8/program31.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class program31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            char ch = 'A';
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++;
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
